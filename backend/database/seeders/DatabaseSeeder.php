a3fdb9d86a390d64b79ea26c33a4aa0405a3f992
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-08-24 01:45:57 +0700

    feat: db:migrate :lock:

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,8 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+       $this->call([RolePermissionSeeder::class, CategorySeeder::class]);
 
-       $this->call(RolePermissionSeeder::class);
-       
     }
 }
